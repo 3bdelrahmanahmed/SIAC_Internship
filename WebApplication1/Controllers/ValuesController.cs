836966e3e7913bfc6dbd95e1e4ea2f63895baa82
Abdelrahman <3bdelrahman.ahmed.fathy@gmail.com>
2024-08-28 16:58:56 +0300

    select with where

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -6,68 +6,89 @@ using System.Data.SqlClient;
 namespace WebApplication1.Controllers
 {
     [ApiController]
-    public class ValuesController : ControllerBase {
-        private  IConfiguration _configuration;
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _sqlDatasource;
+
         public ValuesController(IConfiguration configuration)
         {
             _configuration = configuration;
+            _sqlDatasource = _configuration.GetConnectionString("SIAC_Database");
         }
+
+        private DataTable ExecuteQuery(string query, SqlParameter[] parameters = null)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection mycon = new SqlConnection(_sqlDatasource))
+            {
+                mycon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, mycon))
+                {
+                    if (parameters != null)
+                    {
+                        myCommand.Parameters.AddRange(parameters);
+                    }
+                    using (SqlDataReader myReader = myCommand.ExecuteReader())
+                    {
+                        table.Load(myReader);
+                    }
+                }
+            }
+
+            return table;
+        }
+
         [HttpGet("Get_data")]
         public JsonResult Get_data()
         {
-            string query = "select * from table_1";
-            DataTable table = new DataTable();
-            string sqlDatasource = _configuration.GetConnectionString("SIAC_Database");
-            SqlDataReader myReader;
-            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
+            string query = "SELECT * FROM table_1";
+            DataTable table = ExecuteQuery(query);
+            return new JsonResult(table);
+        }
+        [HttpGet("Get_data_by_id")]
+        public IActionResult Get_data_by_id([FromQuery] string id)
+        {
+            string query = "SELECT * FROM table_1 WHERE PersonID=@id";
+            SqlParameter[] parameters = {
+                new SqlParameter("@id", id)
+            };
+            DataTable table = ExecuteQuery(query, parameters);
+            if (table.Rows.Count == 0)
             {
-                mycon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query , mycon)) { 
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                }
+                return NotFound("ID not found");
             }
             return new JsonResult(table);
         }
+
         [HttpPost("insert_data")]
-        public JsonResult insert_data([FromForm] string Name , string Address , string Age)
+        public JsonResult Insert_data([FromForm] string Name, [FromForm] string Address, [FromForm] string Age)
         {
-            string query = "insert into table_1 values (@N , @D , @A)";
-            DataTable table = new DataTable();
-            string sqlDatasource = _configuration.GetConnectionString("SIAC_Database");
-            SqlDataReader myReader;
-            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
-            {
-                mycon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, mycon))
-                {
-                    myCommand.Parameters.AddWithValue("@N", Name);
-                    myCommand.Parameters.AddWithValue("@D", Address);
-                    myCommand.Parameters.AddWithValue("@A", Age);
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                }
-            }
+            string query = "INSERT INTO table_1 VALUES (@N, @D, @A)";
+            SqlParameter[] parameters = {
+                new SqlParameter("@N", Name),
+                new SqlParameter("@D", Address),
+                new SqlParameter("@A", Age)
+            };
+            ExecuteQuery(query, parameters);
             return new JsonResult("Inserted Successfully");
         }
+
         [HttpDelete("delete_data")]
-        public JsonResult delete_data([FromForm] string id)
+        public IActionResult Delete_data( string id)
         {
-            string query = "delete from table_1 where PersonID=@id";
-            DataTable table = new DataTable();
-            string sqlDatasource = _configuration.GetConnectionString("SIAC_Database");
-            SqlDataReader myReader;
-            using (SqlConnection mycon = new SqlConnection(sqlDatasource))
+            string query = "DELETE FROM table_1 WHERE PersonID=@id";
+            SqlParameter[] parameters = {
+                new SqlParameter("@id", id)
+            };
+            DataTable table = ExecuteQuery("SELECT * FROM table_1 WHERE PersonID=@id", parameters);
+            if (table.Rows.Count == 0)
             {
-                mycon.Open();
-                using (SqlCommand myCommand = new SqlCommand(query, mycon))
-                {
-                    myCommand.Parameters.AddWithValue("@id", id);
-                    myReader = myCommand.ExecuteReader();
-                    table.Load(myReader);
-                }
+                return NotFound("ID not found");
             }
-            return new JsonResult("deleted Successfully");
-        }
+            ExecuteQuery(query, parameters);
+            return Ok("Deleted Successfully");
+        }   
     }
 }
